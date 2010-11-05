b3bc1d908bafeba25f835f4b2395f255462e7499
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2010-11-05 02:40:43 +0100

    Setting up client project.

---
+++ client/src/unabomber/client/Unabomber.java
@@ -0,0 +1,13 @@
+package unabomber.client;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Unabomber extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

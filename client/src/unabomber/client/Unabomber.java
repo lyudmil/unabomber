745e8757216c5c595dad488fb11f7ce3b0d51c6a
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2010-11-07 14:50:33 +0100

    Adding zoom controls to map.

--- client/src/unabomber/client/Unabomber.java
+++ client/src/unabomber/client/Unabomber.java
@@ -3,12 +3,18 @@ package unabomber.client;
 import android.os.Bundle;
 
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 
 public class Unabomber extends MapActivity {
-    @Override
+    private MapView mapView;
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        mapView = (MapView)findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
     }
 
 	@Override

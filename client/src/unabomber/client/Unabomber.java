d38808d8cb98552d63bedaaf472b14f68a9a110a
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2010-12-03 00:01:40 +0100

    Displaying the player's current location.

--- client/src/unabomber/client/Unabomber.java
+++ client/src/unabomber/client/Unabomber.java
@@ -1,12 +1,19 @@
 package unabomber.client;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
 
 public class Unabomber extends MapActivity {
     private MapView mapView;
+	private MyLocationOverlay myLocationOverlay;
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -14,7 +21,10 @@ public class Unabomber extends MapActivity {
         setContentView(R.layout.main);
         
         mapView = (MapView)findViewById(R.id.mapview);
+        myLocationOverlay = new MyLocationOverlay(this, mapView);
+        myLocationOverlay.enableMyLocation();
         mapView.setBuiltInZoomControls(true);
+        mapView.getOverlays().add(myLocationOverlay);
     }
 
 	@Override

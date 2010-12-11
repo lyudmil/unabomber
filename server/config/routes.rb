3300c12b6f970331d441a63cb91f38a7d90f6e6c
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2010-12-11 17:39:52 +0100

    Patching a strange routing bug. We'll figure out a better solution later.

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,8 @@
 Server::Application.routes.draw do
   resources :players, :only => [:create, :show]
   
+  #This makes the client work, so it is necessary. It should be unnecessary and use the resource route defined above.
   post 'players/create' => 'players#create'
+  
   put 'players/:device_id/update' => 'players#update'
 end

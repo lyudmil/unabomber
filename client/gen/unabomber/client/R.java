be3b83f50217057c1536440f3d0d01348ce60dd2
Davide <gadio87@gmail.com>
2011-01-03 21:05:41 +0100

    Added user interface for send player in jail

--- client/gen/unabomber/client/R.java
+++ client/gen/unabomber/client/R.java
@@ -24,5 +24,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int other_player_menu_title=0x7f040001;
+        public static final int sent_to_jail=0x7f040002;
+        public static final int unknown_action=0x7f040003;
     }
 }
--- client/src/ui/OtherPlayersOverlay.java
+++ client/src/ui/OtherPlayersOverlay.java
@@ -1,21 +1,98 @@
 package ui;
 
+import java.util.List;
 
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
+import unabomber.client.R;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
+import android.widget.Toast;
+import engine.GameEngine;
 import engine.PlayerLocation;
 
 public class OtherPlayersOverlay extends UnabomberItemsOverlay {
 	private int myPlayerId;
-
+	private Context mContext;
 	public OtherPlayersOverlay(Drawable defaultMarker, int myPlayerId) {
 		super(defaultMarker);
 		this.myPlayerId = myPlayerId;
 	}
 
-	public void addOverlayFor(PlayerLocation location) {
-		if(displayingMyself(location)) return;
+	public OtherPlayersOverlay(Drawable defaultMarker, int myPlayerId,
+			Context context) {
+		super(defaultMarker);
+		this.myPlayerId = myPlayerId;
+		this.mContext = context;
+	}
+
+	// Handle a tap on other player icons, this is a "system" callback
+	@Override
+	protected boolean onTap(final int index) {
+
+		// option to use as action
+		final CharSequence[] items = { "Send to jail" };
+
 		
-		addItemAt(location.getLocation());
+		// if you have added something, change handleMenuOption
+		AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+		builder.setTitle(R.string.other_player_menu_title);
+		builder.setItems(items, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int item) {
+				handleMenuOption(item, index);
+			}
+		});
+		AlertDialog alert = builder.create();
+		alert.show();
+		return true;
+	}
+
+	// handle the single option that a player has done
+	protected void handleMenuOption(final int optionIndex, final int playerIndex) {
+		
+		
+		//get target player ID
+		int targetPlayerID = Integer.parseInt(locations.get(playerIndex).getSnippet());
+		
+
+		// obtain the user decision
+		switch (optionIndex) {
+		case 0: // send another player to jail
+
+			// TO DO
+
+			//feedback the results
+			Toast.makeText(mContext, R.string.sent_to_jail, Toast.LENGTH_SHORT)
+					.show();
+			break;
+
+		default: // defensive programming
+			Toast.makeText(mContext, R.string.unknown_action,
+					Toast.LENGTH_SHORT).show();
+		}
+
+	}
+
+	//add a player on the list
+	public void addOverlayFor(PlayerLocation location) {
+		
+		//if i am the player then return
+		if (displayingMyself(location))
+			return;
+
+		
+		//else create the overlay item
+		Double latitude = location.getLocation().getLatitude() * 1E6;
+		Double longitude = location.getLocation().getLongitude() * 1E6;
+		GeoPoint point = new GeoPoint(latitude.intValue(), longitude.intValue());
+		
+		//add the playerID as message of the overlay item
+		locations.add(new OverlayItem(point, "Player", String.valueOf(location.getPlayerId())));
 	}
 
 	private boolean displayingMyself(PlayerLocation location) {

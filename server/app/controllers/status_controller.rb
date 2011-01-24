dfa3cd9933aa3006e95838af75fb72bb252d1138
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2011-01-24 19:11:44 +0100

    Can now tell if the player has been arrested and/or killed in the game status. Still no clear idea ow to refactor this awful action.

--- server/app/controllers/status_controller.rb
+++ server/app/controllers/status_controller.rb
@@ -1,29 +1,36 @@
 class StatusController < ApplicationController
   
   def index
-    player = Player.find_by_device_id(params[:device_id])
-    citizens = Player.where(:role => :citizen)
+    @player = player_with_specified_device_id
+    return unless @player
     
-    unless player
+    @citizens = Player.where(:role => :citizen).select { |citizen| citizen.active? }
+    
+    if @player.killed?
       render :text => 'finished-killed'
       return
     end
     
-    if player.role != :unabomber
-      unabombers = Player.where(:role => :unabomber)
+    if @player.arrested?
+      render :text => 'finished-jail'
+      return
+    end
+    
+    if @player.role != :unabomber
+      unabombers = Player.where(:role => :unabomber).select { |unabomber| unabomber.active? }
       if unabombers.empty?
         render :text => 'finished-win' 
         return
       end
       
-      if citizens.empty?
+      if @citizens.empty?
         render :text => 'finished-lose'
         return
       end
     end  
     
-    if player.role == :unabomber
-      if citizens.empty?
+    if @player.role == :unabomber
+      if @citizens.empty?
         render :text => 'finished-win'
         return
       end
--- server/test/functional/status_controller_test.rb
+++ server/test/functional/status_controller_test.rb
@@ -3,22 +3,31 @@ require 'test_helper'
 class StatusControllerTest < ActionController::TestCase
   
   def setup
-    @player = flexmock(:model, Player)
+    @player = flexmock(:model, Player, :active? => true, :killed? => false, :arrested? => false)
     flexmock(Player).should_receive(:find_by_device_id).with('333').and_return(@player)
   end
   
   test "can tell if a player has been killed" do
-    flexmock(Player).should_receive(:find_by_device_id).with('111').and_return(nil)
-    flexmock(Player).should_receive(:where).with(:role => :citizen)
+    player = flexmock(:model, Player, :killed? => true, :arrested? => false)
+    flexmock(Player).should_receive(:find_by_device_id).with('111').and_return(player)
     
     get :index, :device_id => '111'
     
     assert_equal 'finished-killed', @response.body
   end
   
+  test "can tell if a player has been arrested" do
+    player = flexmock(:model, Player, :role => :citizen, :arrested? => true, :killed? => false)
+    flexmock(Player).should_receive(:find_by_device_id).with('222').and_return(player)
+    
+    get :index, :device_id => '222'
+    
+    assert_equal 'finished-jail', @response.body
+  end
+  
   test "can tell if a citizen has won" do
     @player.should_receive(:role).and_return(:citizen)
-    flexmock(Player).should_receive(:where).with(:role => :citizen)
+    flexmock(Player).should_receive(:where).with(:role => :citizen).and_return([@player])
     no_unabombers
     
     get :index, :device_id => '333'
@@ -28,7 +37,7 @@ class StatusControllerTest < ActionController::TestCase
   
   test "can tell if a cop has won" do
     @player.should_receive(:role).and_return(:policeman)
-    flexmock(Player).should_receive(:where).with(:role => :citizen)
+    flexmock(Player).should_receive(:where).with(:role => :citizen).and_return([flexmock(:model, Player, :active? => true)])
     no_unabombers
     
     get :index, :device_id => '333'
@@ -39,7 +48,7 @@ class StatusControllerTest < ActionController::TestCase
   test "can tell if a cop has lost" do
     @player.should_receive(:role).and_return(:policeman)
     no_citizens
-    flexmock(Player).should_receive(:where).with(:role => :unabomber).and_return([flexmock(:model, Player)])
+    flexmock(Player).should_receive(:where).with(:role => :unabomber).and_return([flexmock(:model, Player, :active? => true)])
     
     get :index, :device_id => '333'
     
@@ -56,9 +65,10 @@ class StatusControllerTest < ActionController::TestCase
   end
   
   test "can tell if a game is still in progress" do
+    @player.should_receive(:killed?).and_return(false)
     @player.should_receive(:role).and_return(:citizen)
-    flexmock(Player).should_receive(:where).with(:role => :citizen).and_return([@player, flexmock(:model, Player)])
-    flexmock(Player).should_receive(:where).with(:role => :unabomber).and_return([flexmock(:model, Player)])
+    flexmock(Player).should_receive(:where).with(:role => :citizen).and_return([@player, flexmock(:model, Player, :active? => true)])
+    flexmock(Player).should_receive(:where).with(:role => :unabomber).and_return([flexmock(:model, Player, :active? => true)])
     
     get :index, :device_id => '333'
     
@@ -68,11 +78,13 @@ class StatusControllerTest < ActionController::TestCase
   private
   
   def no_unabombers
-    flexmock(Player).should_receive(:where).with(:role => :unabomber).and_return([])
+    arrested_unabomber = flexmock(:model, Player, :role => :unabomber, :active? => false)
+    flexmock(Player).should_receive(:where).with(:role => :unabomber).and_return([arrested_unabomber, arrested_unabomber])
   end
   
   def no_citizens
-    flexmock(Player).should_receive(:where).with(:role => :citizen).and_return([])
+    dead_citizen = flexmock(:model, Player, :role => :citizen, :active? => false)
+    flexmock(Player).should_receive(:where).with(:role => :citizen).and_return([dead_citizen, dead_citizen])
   end
   
 end
